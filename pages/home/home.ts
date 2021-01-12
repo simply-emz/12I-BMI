88783e59494accda6a90c1b0ea942ef2e1eb6fb0
simply-emz <emie.zipagan@lsgh.edu.ph>
2021-01-12 10:01:20 +0800

    1 file modified, 1 added

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html",
+  styleUrls: ["home.css"]
 })
 export class HomePage {
   height: number;
